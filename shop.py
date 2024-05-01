aef14cc97487c49928410b4b06863c47a2b09fa0
Adam Nix <150264616+realadamnix@users.noreply.github.com>
2024-05-02 00:53:11 +0100

    shop

---
+++ shop.py
@@ -0,0 +1,12 @@
+tax = 0.06
+price_of_milk = 2.50
+price_of_bread = 1.20
+
+print ("please enter the number of milk cartons you want to buy:")
+price_of_milk = int(input())
+
+print ("please enter the number of bread loaves you want to buy:")
+price_of_bread = int(input())
+
+total = (price_of_milk + price_of_bread) * (1 + tax)
+print ("The total price is:" , total , "euros")
