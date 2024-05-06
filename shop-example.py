93580c643aca10a318adbfca51d25cb61b58de23
Adam Nix <150264616+realadamnix@users.noreply.github.com>
2024-05-06 22:46:43 +0100

    Update

--- shop-example.py
+++ shop-example.py
@@ -1,12 +1,16 @@
+"""
+This is a shop example program in python
+Copyright Adam Nix
+"""
 tax = 0.06
 price_of_milk = 2.50
 price_of_bread = 1.20
 
-print ("please enter the number of milk cartons you want to buy:")
+print("please enter the number of milk cartons you want to buy:")
 price_of_milk = int(input())
 
-print ("please enter the number of bread loaves you want to buy:")
+print("please enter the number of bread loaves you want to buy:")
 price_of_bread = int(input())
 
 total = (price_of_milk + price_of_bread) * (1 + tax)
-print ("The total price is:" , total , "euros")
+print("The total price is:", total, "euros")
